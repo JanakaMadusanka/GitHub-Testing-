d91eafa8729cce0a431bb99e94d600996103e730
janaka <jnk.madusanka86@gmail.com>
2023-12-25 15:34:59 +0530

    adding list item

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("js testing");
+document.getElementById('btn-add-item').addEventListener('click',()=>{
+    let itemInput = document.getElementById('input-item');
+    let itemList = document.getElementById('item-list');
+    if(itemInput.value != ""){
+        let newItem = createItemList(itemInput.value);
+        itemList.appendChild(newItem);
+    }
+    
+})
+
+let createItemList = (text)=>{
+    const listItem = document.createElement("li");
+    listItem.textContent = text;
+    return listItem;
+}
